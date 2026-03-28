5ccfbfafa0812c9c6a450c387bed45a3b0357533
Leandro <joseph.trejos@hotmail.com>
2026-03-27 22:37:18 -0500

    Elimino proyecto Next, creo rama de componente landing

--- first_activity_vite/src/App.jsx
+++ first_activity_vite/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
@@ -21,12 +20,7 @@ function App() {
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        
       </section>
 
       <div className="ticks"></div>
